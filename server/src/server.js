54ea1e7de06fb7f0b73426d73d4108009e021b97
irohanrajput <itusharsingh27@gmail.com>
2024-06-02 05:26:55 +0530

    updated some stuff in the schema; added routes; did server configurations etc

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+})
